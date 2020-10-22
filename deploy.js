52c8bf67497a6493d8f289f17e36766437b8a8ab
i-n-g-o <ingo.randolf@servus.at>
2020-10-22 14:48:09 +0200

    deal with more than one deployment target

--- deploy.js
+++ deploy.js
@@ -2,7 +2,25 @@ var config = require('./config');
 var FtpDeploy = require("ftp-deploy");
 var ftpDeploy = new FtpDeploy();
 
-ftpDeploy
-    .deploy(config)
-    .then(res => console.log("finished:", res))
-    .catch(err => console.log(err));
+//default config
+var conf = config.default;
+
+if (process.argv.length > 2)
+{
+    conf = config[process.argv[process.argv.length - 1]];
+}
+
+if (conf !== undefined)
+{
+    console.log("deploying to: " + conf.host);
+    
+    ftpDeploy
+        .deploy(conf)
+        .then(res => console.log("finished:", res))
+        .catch(err => console.log(err));
+}
+else
+{
+    console.error("no such config: " + process.argv[process.argv.length - 1]);
+}
+
