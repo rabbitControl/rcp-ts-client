dff0688c0d545c7c7371053bc90fe84eba3718e9
i-n-g-o <ingo.randolf@servus.at>
2023-04-16 12:38:26 +0200

    version 0.1.7

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ class App extends React.Component<Props, State> {
       height: window.innerHeight,
     };
     
-    console.log("rabbitcontrol web client - version: 0.1.6");
+    console.log("rabbitcontrol web client - version: 0.1.7");
   }
 
   componentDidMount() 
